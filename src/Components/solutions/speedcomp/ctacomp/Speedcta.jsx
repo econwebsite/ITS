2283d030e8f0d0c56493dff9e37ee6d5596775e8
Arthi <marketing@e-consystems.com>
2026-04-07 17:34:34 +0530

    css filenames update

--- src/Components/solutions/speedcomp/ctacomp/Speedcta.jsx
+++ src/Components/solutions/speedcomp/ctacomp/Speedcta.jsx
@@ -1,4 +1,4 @@
-import './Styles.css';
+import './Ctastyles.css';
 
 
 
--- src/Components/solutions/trafficeEnforcement/bannercomp/itsbanner.jsx
+++ src/Components/solutions/trafficeEnforcement/bannercomp/itsbanner.jsx
@@ -1,4 +1,4 @@
-import './Styles.css';
+import './styles.css';
 
 const TrafficSVG = () => (
   <svg viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
